bb257ef2466c4d709ee169735a11821d7e00d7af
Katsa <gykatsaounis@gmail.com>
2014-01-19 22:54:37 -0500

    css edits

--- signuppage.php
+++ signuppage.php
@@ -5,8 +5,8 @@
 
 			Email: <input type= "text" name = "email" required><br>
 			Password: <input type="password" name="pessword" required><br>
-			Confirm Password: <input type = "password" name="confirm" required><br>
-			First Name: <input type = "text" name = "first_name" requited><br>
+			<!--Confirm Password: <input type = "password" name="confirm" required><br>-->
+			First Name: <input type = "text" name = "first_name" required><br>
 			Last Name: <input type = "text" name = "last_name" required><br>
 			<input type = "submit" value = "Sign Up"><br>
 
