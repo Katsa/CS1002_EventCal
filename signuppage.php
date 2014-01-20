04fe467775f8f4691984f54520055b865a29bd97
Katsa <gykatsaounis@gmail.com>
2014-01-20 10:37:42 -0500

    played with signup button

--- signuppage.php
+++ signuppage.php
@@ -5,12 +5,12 @@
 
 			Email: <input type= "text" name = "email" required><br>
 			Password: <input type="password" name="pessword" required><br>
-			<!--Confirm Password: <input type = "password" name="confirm" required><br>-->
+			Confirm Password: <input type = "password" name="confirm" required><br>
 			First Name: <input type = "text" name = "first_name" required><br>
 			Last Name: <input type = "text" name = "last_name" required><br>
-			<input type = "submit" value = "Sign Up"><br>
-
+			<input type="submit" class="btn btn-primary" value="Sign Up"/>
 		</form>
+		
 
 	</body>
 </html>
