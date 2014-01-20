acdcaaf01a39d0baa2de16669c844dec0ebd5b3c
Junha Kim <junhak@breadloaf.cs.middlebury.edu>
2014-01-20 11:14:08 -0500

    minor edits

--- signup.php
+++ signup.php
@@ -1,24 +1,29 @@
 <html>
 <head>
 	<?php
-		define('DB_SERVER', 'panther.cs.middlebury.edu');
-		define('DB_USERNAME', 'jcepeda');
-		define('DB_PASSWORD', 'ForRealThough');
-		define('DB_DATABASE', 'jcepeda_middCal');
-		
-		$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die ("Could not connect");
-
-		$sql = "INSERT INTO Users (password, email, first_name, last_name)
-		VALUES
-		('$_POST[password]', '$_POST[email]', '$_POST[first_name]', '$_POST[last_name]')");
-		
-		if (!mysqli_query($con, $sql)) {
-			die('Error: ' . mysqli_error($con));
+		if ($_POST[password] != $_POST[confirm]) {
+			echo "Passwords do not match";
 		}
+		else {
+			define('DB_SERVER', 'panther.cs.middlebury.edu');
+			define('DB_USERNAME', 'jcepeda');
+			define('DB_PASSWORD', 'ForRealThough');
+			define('DB_DATABASE', 'jcepeda_middCal');
+			
+			$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die ("Could not connect");
 
-		echo "Account created";
+			$sql = "INSERT INTO Users (password, email, first_name, last_name)
+			VALUES
+			('$_POST[password]', '$_POST[email]', '$_POST[first_name]', '$_POST[last_name]')");
+			
+			if (!mysqli_query($con, $sql)) {
+				die('Error: ' . mysqli_error($con));
+			}
 
-		mysql_close($con)
+			echo "Account created";
+
+			mysql_close($con)
+		}
 
 	?>
 	<meta http-equiv = "refresh" content" 3; URL=http://www.cs.middlebury.edu/~gkatsaounis/CS1002_EventCal/main.php">
