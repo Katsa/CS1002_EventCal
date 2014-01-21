9fbb1afe5ba00a9cfd3c6bc303ea8af99c70ab96
Junha Kim <junhak@hogback.cs.middlebury.edu>
2014-01-21 16:34:20 -0500

    updates

--- create_event.php
+++ create_event.php
@@ -9,22 +9,22 @@
 	
 	$con = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE) or die ("Could not connect");
 
-	$sql = "INSERT INTO Events (title, location, start_time, end_time, waiting_for_approval, description)
+	$sql = "INSERT INTO Events (title, location, start_date, end_date, start_time, end_time, waiting_for_approval, description)
 	VALUES
-	('$_POST[event_name]', '$_POST[location]', '$_POST[start_time]', '$_POST[end_time]', '0', '$_POST[description]')";
+	('$_POST[event_name]', '$_POST[location]', '$_POST[start_date]', '$_POST[end_date]', '$_POST[start_time]', '$_POST[end_time]', '0', '$_POST[description]')";
 
 	if (!mysqli_query($con, $sql)) {
 		die('Error: ' . mysqli_error($con));
 	}
 
-	echo "Event submission successful.  Your event is now pending approval.  Your browser will automatically redirect you in 10 seconds.";
+	echo "Event submission successful.  Your event is now pending approval.  Your browser will automatically redirect you in 3 seconds.";
 	
 
 	mysql_close($con)
 	
 ?>
 
-<meta http-equiv="refresh" content="10"; URL="main.html">
+<meta http-equiv="refresh" content="3" URL="main.html">
 
 </head>
 </html>
