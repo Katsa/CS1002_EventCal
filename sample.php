21771e045fbde6ab7eff09381875f1052cd6bb57
Katsa <gykatsaounis@gmail.com>
2014-01-28 19:00:29 -0500

    fixed link to sample/search page

--- sample.php
+++ sample.php
@@ -26,44 +26,10 @@
 </head>
 
 <body>
-    <div>
-        <nav class="navbar navbar-fixed-top navbar-inverse" role="navigation">
-            <div class="container">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                    <a class="navbar-brand" href="main.php">MiddLife</a>
-                </div>
-                <div class="collapse navbar-collapse navbar-ex1-collapse">
-                    <ul class="nav navbar-nav row">
-                        <li class="dropdown">
-                        <a href="sample.php">Search</a>
-                        
-                    </li>
-                    <?php
-                        if($_SESSION["email"] != "") { //if logged in
-                            echo '<li> <a href="create_event.php">Create an Event</a> </li> <li><a href = "logout.php">Logout</a></li>';
-                        }
-                        else { //if not logged in
-                            echo '<li><a href="login.php" class="btn-login login">Login</a> </li> <li><a href="signup.php">Sign Up</a>';
-                        }
-                        if ($_SESSION["admin"] == "1") { //if admin
-                            echo '<li><a href="approve.php">Approve</a> </li>';
-                        }
-                    echo                
-                    ?>
-                </ul>
-                </div>
-            </div>
-        </nav>
-    </div>
-<br>
+    <?php include 'include/navbar.php'; ?>
 
-<div class="container">
+
+<div class="container top">
   <legend>Search</legend>
     <div class="row">
       <div class="col-md-6 col-md-offset-3">
