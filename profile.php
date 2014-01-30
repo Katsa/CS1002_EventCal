0d50b05456db4faca490642d4f38156445db1d0a
Katsa <gykatsaounis@gmail.com>
2014-01-30 15:11:28 -0500

    name and last name next to each other

--- profile.php
+++ profile.php
@@ -46,9 +46,7 @@
                 </div>
                     <div class="col-md-9 row">
                         <div class="col-xs-6 col-md-6 row span4">
-                            <h4 > <?php echo "$data[first_name]";?></h4>
-                        
-                            <h4> <?php echo "$data[last_name]";?></h4> <!-- This is where the last name will go -->
+                            <h4 > <?php echo "$data[first_name] $data[last_name]";?></h4>
                                                
                         <br>
                         <br>
