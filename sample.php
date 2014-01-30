6774d6acf419c57502b0f881133e4f1273a36bd9
Katsa <gykatsaounis@gmail.com>
2014-01-30 16:50:23 -0500

    add sample

---
+++ sample.php
@@ -0,0 +1,98 @@
+<?php session_start(); ?>
+
+<html>
+<head>
+   <?php 
+    include 'include/header.php'; 
+    ?>
+     <link rel="stylesheet" type="text/css" href="css/sample.css">
+
+</head>
+
+<body>
+    <?php include 'include/navbar.php'; ?>
+
+    <div class="container top">
+        <legend>Sample</legend>
+            <div class="row">
+                <div class="col-md-4 search-form steady">
+                    <h3>Search by:</h3>
+
+                    <form class="navbar-form navbar-left" role="search" action="results.php">
+                        <div class="form-group">
+                            <input name = "creator" type="text" class="form-control" placeholder="Creator's Name...">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Search...</button>
+                    </form>
+
+                    <form class="navbar-form navbar-left" role="search" action="results.php">
+                        <div class="form-group">
+                            <input name = "creator" type="text" class="form-control" placeholder="Event's Title...">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Search...</button>
+                    </form>
+
+                    <form class="navbar-form navbar-left" role="search" action="results.php">
+                        <div class="form-group">
+                            <input name = "creator" type="text" class="form-control" placeholder="Location of Event...">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Search...</button>
+                    </form>
+
+                    <form class="navbar-form navbar-left" role="search" action="results.php">
+                        <div class="form-group">
+                            <input name = "creator" type="text" class="form-control" placeholder="yyyy-mm-dd">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Search...</button>
+                    </form>
+
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input name = "creator" type="text" class="form-control" placeholder="tag1 tag2 tag3 tag4...">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Search...</button>
+                    </form>
+            </div>
+            <div class="col-md-6 col-md-offset-4">
+                <h3>Results:</h3>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+                <p>kjhagsfdoiagshdahsdpashpd</p>
+
+            </div>
+        </div>
+    </div>
+</body>
+</html>
